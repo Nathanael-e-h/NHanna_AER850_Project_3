e4d2ba3b799747d28b4cf13fc4577338d567a38f
Nathanael Hanna <128150666+Nathanael-e-h@users.noreply.github.com>
2024-11-28 10:34:15 -0500

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+
+# print("\nEnter a number: ")
+
+number = int(input("\n Give me a number: "))
+
+print("Your number is", number,".\nIf you squared it, it would be", number*number, ".")
