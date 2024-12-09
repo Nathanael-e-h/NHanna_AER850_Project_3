59befd8b9d051c90ec9f6ff6ad38ef81ead2f9e9
Nathanael Hanna <128150666+Nathanael-e-h@users.noreply.github.com>
2024-12-08 19:28:06 -0500

    Dec_8_Evening
    Finally got started and, although it took a painful amount of time, I got all the way through step 1. As per *everyone's* suggestions, I'm doing this in Google Colab to slurp up that sweet, sweet Quadro T4 juice. I'll start step 2 after my exams tomorrow.
    
    NOTE TO SELF: Gotta figure out how to/if I can use that .yaml file to import all the stuffz.

---
+++ NHanna_AER850_Project3.py
@@ -0,0 +1,5 @@
+##### PROJECT 3: YOLO Object Detection 
+
+# First part is involved; use contours not edge detections (TA never got it to work)
+
+# 2nd is braindead simple; like four lines of code.
